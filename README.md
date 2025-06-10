6b27f4273a8a9ea87dcb2e593cf365d1760f7c89
Thomas McMahon <tmcmahon@nerdery.com>
2025-06-10 08:55:39 -0500

    Add initial implementation of Simple HubSpot Forms Integration plugin with shortcode support

---
+++ README.md
@@ -0,0 +1,47 @@
+# Simple HubSpot Forms Integration WordPress Plugin
+
+A lightweight WordPress plugin that allows you to easily embed HubSpot forms into your WordPress pages and posts using shortcodes.
+
+This plugin can replace the [official HubSpot plugin](https://wordpress.org/plugins/leadin/) if you want something more lightweight. It'll work with the same shortcodes, just deactivate the official plugin and activate this one. There are no HubSpot integrations other than embedding a form.
+
+## Installation
+
+1. Download the plugin files
+2. Upload the `hubspot-forms` folder to your `/wp-content/plugins/` directory
+3. Activate the plugin through the 'Plugins' menu in WordPress
+
+## Usage
+
+Use the shortcode `[hubspot]` in your posts or pages to embed a HubSpot form. The shortcode accepts two parameters:
+
+- `id` (required): The HubSpot Form ID
+- `portal` (required): Your HubSpot Portal ID
+
+### Example
+
+```
+[hubspot portal="12345678" id="nrd5c5e0-6f2f-46b1-95d7-aacb3975e9df"]
+```
+
+### Finding Your Form and Portal IDs
+
+1. In HubSpot, navigate to Marketing → Lead Capture → Forms
+2. Select your form
+3. Click the "Share" button
+4. In the embed code, you'll find both your Portal ID and Form ID
+
+## Features
+
+- Simple shortcode implementation
+- Respects HubSpot cookies
+- Unique container IDs for multiple forms on the same page
+- Secure output with WordPress escaping functions
+
+## Requirements
+
+- WordPress 4.0 or higher
+- Active HubSpot account
+
+## License
+
+This plugin is licensed under GPL v2 or later.
---
+++ hubspot-forms.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Plugin Name: Simple HubSpot Forms Integration
+ * Description: Adds a shortcode to embed HubSpot forms on your WordPress site.
+ * Version: 0.1.0
+ * Author: Thomas McMahon
+ * License: GPL v2 or later
+ */
+
+// Prevent direct access to this file
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Register the HubSpot form shortcode
+ */
+function hubspot_form_shortcode($atts) {
+    // Extract shortcode attributes
+    $atts = shortcode_atts(
+        array(
+            'id' => '', // Form ID attribute
+            'portal' => '', // Portal ID attribute with default
+        ),
+        $atts,
+        'hubspot'
+    );
+
+    // If no form ID is provided, return an error message
+    if (empty($atts['id'])) {
+        return 'Error: HubSpot form ID is required.';
+    }
+
+    // Generate a unique container ID for this form instance
+    $container_id = 'hubspot-form-' . uniqid();
+
+    // Create the form container and script
+    $output = '<div id="' . esc_attr($container_id) . '" class="contact-us-form-shortcode">';
+    $output .= '<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>';
+    $output .= '<script>
+        hbspt.forms.create({
+            portalId: "' . esc_js($atts['portal']) . '",
+            formId: "' . esc_js($atts['id']) . '",
+            region: "na1",
+            target: "#' . esc_js($container_id) . '"
+        });
+    </script></div>';
+
+    return $output;
+}
+
+// Register the shortcode
+add_shortcode('hubspot', 'hubspot_form_shortcode');
